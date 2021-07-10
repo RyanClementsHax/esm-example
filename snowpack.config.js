cb6527eecb0904f6580c744bf9a3e5df8b50122f
Ryan Clements <ryanclementshax@gmail.com>
2021-07-10 11:50:11 -0400

    create build script with webpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -10,7 +10,24 @@ module.exports = {
     },
     src: "/dist"
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript', '@snowpack/plugin-sass'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass',
+    [
+      '@snowpack/plugin-webpack',
+      {
+        htmlMinifierOptions: false
+      }
+    ]
+  ],
+  // experiments: {
+  //   optimize: {
+  //     bundle: true,
+  //     minify: true,
+  //     target: 'es2018'
+  //   }
+  // }
   // installOptions: {},
   // devOptions: {},
   // buildOptions: {},
